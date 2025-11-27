d4e6a73e8e98623aa546aa1700971ad677488457
fidha-jazeel <fidhashraf001@gmail.com>
2025-11-27 13:45:50 +0400

    code conflict solved

--- travel_billing_software/auth/__init__.py
+++
@@ -1,6 +0,0 @@
-"""
-Authentication module for billing software
-"""
-from travel_billing_software.auth.auth_manager import AuthManager
-
-__all__ = ['AuthManager']
--- travel_billing_software/ui/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-UI Package - Travel Agency Billing Software
-Contains all user interface components
-"""
-from .login_page import LoginPage
-from .change_password_dialog import ChangePasswordDialog
-
-__all__ = ['LoginPage', 'ChangePasswordDialog']
